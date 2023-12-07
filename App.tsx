2cb5e6cb2f396e07116cd85dd1755a6bd536f94f
toluasalu <johnasalu13@gmail.com>
2023-12-07 05:56:37 +0100

    Advent Calendar now displays dates in a box

--- App.tsx
+++ App.tsx
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import Box from './src/components/Box';
+
+const days = [...Array(24)].map((val, index) => index + 1);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <FlatList
+        data={days}
+        contentContainerStyle={styles.content}
+        columnWrapperStyle={styles.column}
+        numColumns={2}
+        renderItem={({ item }) => (
+          <Box value={item} />
+        )}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content: {
+    gap: 10,
+    padding: 10
+  },
+  column: {
+    gap: 10
+  },
+  box: {
+    backgroundColor: "#F9EDE3",
+    flex: 1,
+    aspectRatio: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#9b4931",
+    borderWidth: StyleSheet.hairlineWidth
+  },
+  text: {
+    color: "#9b4931",
+    fontSize: 60,
+  }
 });
---
+++ src/components/Box.tsx
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Box = ({ value }: any) => {
+    return (
+        <View style={styles.box}>
+            <Text style={styles.text}>{value}</Text>
+        </View>
+    )
+}
+
+export default Box
+
+
+const styles = StyleSheet.create({
+
+    box: {
+        backgroundColor: "#F9EDE3",
+        flex: 1,
+        aspectRatio: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        borderColor: "#9b4931",
+        borderWidth: StyleSheet.hairlineWidth
+    },
+    text: {
+        color: "#9b4931",
+        fontSize: 60,
+    }
+});
